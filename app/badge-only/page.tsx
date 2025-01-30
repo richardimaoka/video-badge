f360a950a2cfb5118467728b6ee3f8558352f511
richardimaoka <richard.s.imaoka@gmail.com>
2025-01-31 05:25:23 +0900

    update badge

---
+++ app/badge-only/page.tsx
@@ -0,0 +1,10 @@
+import { Badge } from "../components/Badge";
+import styles from "./page.module.css";
+
+export default function Page() {
+  return (
+    <div className={styles.component}>
+      <Badge number={2} />
+    </div>
+  );
+}
